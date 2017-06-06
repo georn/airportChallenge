c0eb5f742f849a92ebb42dc4f101ba6eafb05e24
Guillermo <mrog0@outlook.com>
2017-06-06 13:20:14 +0100

    feature testing setup

---
+++ jasmine-standalone/spec/features/featuresSpec.js
@@ -0,0 +1,13 @@
+// describe('Landing a plane in the airport', function() {
+//
+//   var plane;
+//   var airport;
+//
+//   beforeEach(function() {
+//     plane = new Plane();
+//     airport = new Airport();
+//   });
+//
+//
+//
+// });
